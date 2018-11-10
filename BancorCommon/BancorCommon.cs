084ad24a817da48122d5cc231c22617ac65678d0
ProDog <xiuboard@foxmail.com>
2018-11-10 21:01:45 +0800

    增加通用Bancor合约

---
+++ BancorCommon/BancorCommon.cs
@@ -0,0 +1,232 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using Neo.SmartContract.Framework.Services.System;
+using System;
+using System.Numerics;
+using Helper = Neo.SmartContract.Framework.Helper;
+
+namespace BancorCommon
+{
+    /// <summary>
+    /// 通用Bancor合约，实现一个所有智能代币公用的Bancor合约，抵押币统一为BCP
+    /// </summary>
+    public class BancorCommon : SmartContract
+    {
+        //Math合约
+        [Appcall("9f8d9b7dd380c187dadb887a134bf56e3e1d3453")]
+        static extern object mathCall(string method, object[] arr);
+        
+        //总管理员账户 用来设置白名单等
+        static readonly byte[] superAdmin = Helper.ToScriptHash("AGZqPBPbkGoVCQTGSpcyBZRSWJmvdbPD2s");
+        delegate object deleCall(string method, object[] args);
+
+        public static object Main(string method, object[] args)
+        {
+            string magicStr = "BancorCommon";
+
+            if (Runtime.Trigger == TriggerType.Verification)
+            {
+                return false;
+            }
+            else if (Runtime.Trigger == TriggerType.Application)
+            {
+                var callscript = ExecutionEngine.CallingScriptHash;
+                //invoke
+                if (method == "getWhiteList")
+                    return GetWhiteList();
+                
+                if (method == "calculatePurchaseReturn")
+                {
+                    var assetid = (byte[]) args[0];
+                    var amount = (BigInteger)args[1];
+                    var assetInfo = GetAssetInfo(assetid);
+                    if (amount == 0 || assetInfo.connectBalance == 0 || assetInfo.smartTokenSupply == 0 ||
+                        assetInfo.connectWeight == 0)
+                        return 0;
+                    return mathCall("purchase",
+                        new object[5]
+                        {
+                            amount, assetInfo.connectBalance, assetInfo.smartTokenSupply, assetInfo.connectWeight,
+                            assetInfo.maxConnectWeight
+                        });
+                }
+
+                //总管理员权限
+                if (method == "setWhiteList")
+                {
+                    if (!Runtime.CheckWitness(superAdmin))
+                        return false;
+                    var assetid = (byte[]) args[0];
+                    var connectAssetId = (byte[]) args[1];
+                    var admin = (byte[]) args[2];
+                    if (assetid.Length == 0 || connectAssetId.Length == 0 || admin.Length == 0)
+                        return false;
+                    SetWhiteList(assetid, admin);
+                }
+
+                //应用币管理员权限
+                if (method == "setConnectAssetId")
+                {
+                    var assetid = (byte[])args[0];
+                    var connectAssetId = (byte[])args[1];
+                    var admin = GetAdmin(assetid);
+                    if (!Runtime.CheckWitness(admin))
+                        return false;
+                    if (assetid.Length == 0 || connectAssetId.Length == 0)
+                        return false;
+                    var assetInfo = GetAssetInfo(assetid);
+                    assetInfo.connectAssetId = connectAssetId;
+                    return SetAssetInfo(assetid, assetInfo);
+                }
+
+                if (method == "setConnectWeight")
+                {
+                    var assetid = (byte[]) args[0];
+                    var admin = GetAdmin(assetid);
+                    if (!Runtime.CheckWitness(admin))
+                        return false;
+                    var connectWeight = (BigInteger) args[1];
+                    var assetInfo = GetAssetInfo(assetid);
+                    assetInfo.connectWeight = connectWeight;
+                    SetAssetInfo(assetid, assetInfo);
+                }
+
+                if (method == "setConnectBalanceIn")
+                {
+                    var assetid = (byte[]) args[0];
+                    var txid = (byte[]) args[1];
+                    var assetInfo = GetAssetInfo(assetid);
+                    var admin = GetAdmin(assetid);
+                    if (!Runtime.CheckWitness(admin))
+                        return false;
+                    if (assetid.Length == 0 || txid.Length == 0 || assetInfo.connectAssetId.Length == 0)
+                        return false;
+                    var tx = GetTxInfo(assetid, txid);
+                    if (tx.from.Length == 0 || tx.from.AsBigInteger() != admin.AsBigInteger())
+                        return false;
+                    if (tx.to.AsBigInteger() != ExecutionEngine.ExecutingScriptHash.AsBigInteger())
+                        return false;
+                    if (tx.value <= 0)
+                        return false;
+                    assetInfo.connectBalance += tx.value;
+                    SetAssetInfo(assetid, assetInfo);
+                    SetTxUsed(txid);
+                    return true;
+                }
+
+                if (method == "setSmartTokenSupplyIn")
+                {
+                    var assetid = (byte[])args[0];
+                    var txid = (byte[])args[1];
+                    var tx = GetTxInfo(assetid, txid);
+                    var admin = GetAdmin(assetid);
+                    if (tx.from.Length == 0 || tx.from.AsBigInteger() != admin.AsBigInteger())
+                        return false;
+                    if (tx.to.AsBigInteger() != ExecutionEngine.ExecutingScriptHash.AsBigInteger())
+                        return false;
+                    if (tx.value <= 0)
+                        return false;
+                    var assetInfo = GetAssetInfo(assetid);
+                    assetInfo.smartTokenSupply += tx.value;
+                    SetAssetInfo(assetid, assetInfo);
+                    SetTxUsed(txid);
+                    return true;
+                }
+
+            }
+
+            return false;
+        }
+
+        public static object SetAssetInfo(byte[] assetid, AssetInfo assetInfo)
+        {
+            StorageMap assentInfoMap = Storage.CurrentContext.CreateMap("assentInfoMap");
+            byte[] assetInfoBytes = Helper.Serialize(assetInfo);
+            assentInfoMap.Put(assetid, assetInfoBytes);
+            return true;
+        }
+
+        public static AssetInfo GetAssetInfo(byte[] assetid)
+        {
+            StorageMap assentInfoMap = Storage.CurrentContext.CreateMap("assentInfoMap");
+            byte[] assetInfoBytes = assentInfoMap.Get(assetid);
+            AssetInfo assetInfo = new AssetInfo();
+            if (assetInfoBytes.Length > 0)
+                assetInfo = assetInfoBytes.Deserialize() as AssetInfo;
+            return assetInfo;
+        }
+
+        /// <summary>
+        /// 设置可以使用Bancor的白名单
+        /// </summary>
+        /// <param name="key">nep5资产hash</param>
+        /// <param name="admin">代币管理员地址</param>
+        /// <returns></returns>
+        public static bool SetWhiteList(byte[] key, byte[] admin)
+        {
+            StorageMap whiteListMap = Storage.CurrentContext.CreateMap("whiteListMap");
+            byte[] whiteListBytes = whiteListMap.Get("whiteList");
+            Map<byte[], byte[]> map = new Map<byte[], byte[]>();
+            if (whiteListBytes.Length > 0)
+                map = whiteListBytes.Deserialize() as Map<byte[], byte[]>;
+            map[key] = admin;
+            whiteListMap.Put("whiteList", map.Serialize());
+            return true;
+        }
+
+        public static Map<byte[], byte[]> GetWhiteList()
+        {
+            StorageMap whiteListMap = Storage.CurrentContext.CreateMap("whiteListMap");
+            byte[] data = whiteListMap.Get("whiteList");
+            if (data.Length == 0)
+                return new Map<byte[], byte[]>();
+            return data.Deserialize() as Map<byte[], byte[]>;
+        }
+
+        public static byte[] GetAdmin(byte[] assetid)
+        {
+            var map = GetWhiteList();
+            return map[assetid];
+        }
+
+        public static TransferInfo GetTxInfo(byte[] assetid, byte[] txid)
+        {
+            deleCall call = (deleCall)assetid.ToDelegate();
+            StorageMap txInfoMap = Storage.CurrentContext.CreateMap("txInfoMap");
+            var tInfo = new TransferInfo();
+            var v = txInfoMap.Get(txid).AsBigInteger();
+            if (v == 0)
+            {
+                object[] _p = new object[1] { txid };
+                var info = call("getTxInfo", _p);
+                if (((object[])info).Length == 3)
+                    tInfo = info as TransferInfo;
+            }
+            return tInfo;
+        }
+
+        public static void SetTxUsed(byte[] txid)
+        {
+            StorageMap txInfoMap = Storage.CurrentContext.CreateMap("txInfoMap");
+            txInfoMap.Put(txid, 1);
+        }
+
+    }
+
+    public class TransferInfo
+    {
+        public byte[] from;
+        public byte[] to;
+        public BigInteger value;
+    }
+
+    public class AssetInfo
+    {
+        public byte[] connectAssetId;
+        public byte[] admin;
+        public BigInteger connectWeight;
+        public BigInteger maxConnectWeight;
+        public BigInteger connectBalance;
+        public BigInteger smartTokenSupply;
+    }
+}
